8ca29b5555094b7dceadcf4402c736156ee54870
wtvieira <william.trindade010@gmail.com>
2025-02-18 15:43:25 -0300

    feat: primeira implementação de rota

---
+++ src/main/routes/event.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, jsonify 
+
+event_route_bp = Blueprint("event_route", __name__)
+
+
+@event_route_bp.route("/event", methods=["POST"])
+def create_new_event():
+    return jsonify({"estou": "aqui"}), 201
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,3 +1,7 @@
 from flask import Flask
 
+from src.main.routes.event import event_route_bp
+
 app = Flask(__name__)
+
+app.register_blueprint(event_route_bp)
